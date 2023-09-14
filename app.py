84f4c160d6abfa5eac102fa451d4d3d41b50388e
PRAJNA R PATIL <93109368+prajna0906@users.noreply.github.com>
2023-09-14 12:35:49 +0530

    Feat: Add Text chunks

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from utils import load_ui, get_text_chunks, get_pdf_text
 def main():
     load_dotenv()
     raw_text = get_pdf_text(path="./dataset")
+    text_chunks = get_text_chunks(raw_text)
 
 if __name__ == "__main__":
     main()
