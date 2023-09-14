56c7f2dff7c1d9ce306b39f205a647c9c4655b75
antidoid <singhpiyush998@gmail.com>
2023-09-14 10:35:12 +0530

    Add: text_chunks code

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 
-from utils import load_ui
+from utils import load_ui, get_text_chunks
 
 def main():
     load_dotenv()
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,2 @@
 from .ui import load_ui
+from .text_chunks import get_text_chunks
